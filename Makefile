28e6ca171174389143bc485a30313c405d932ea2
Peter Lesslie <pclesslie@gmail.com>
2013-10-17 12:53:02 -0500

    Fully functional but not seeing double attacks
    The AI works and it defends and mate attacks, but it isn't trying to
    use double attacks to win.  Currently if the moves don't lose then it
    just takes the lowest number move.

--- Makefile
+++ Makefile
@@ -2,13 +2,14 @@ CC=gcc
 CFLAGS=-Wall -g
 
 all: main.o
-	$(CC) $(CFLAGS) main.o Board.o TTTIO.o -o tictac
-	./tictac
-main.o: Board.o TTTIO.o
+	$(CC) $(CFLAGS) main.o Board.o TTTIO.o AI.o -o tictac
+main.o: Board.o TTTIO.o AI.o
 	$(CC) $(CFLAGS) -c main.c
 Board.o:
 	$(CC) $(CFLAGS) -c Board.c
 TTTIO.o: Board.o
 	$(CC) $(CFLAGS) -c TTTIO.c
+AI.o:
+	$(CC) $(CFLAGS) -c AI.c
 clean:
 	rm -rf *.o *~ tictac
--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 #include "Board.h"
 #include "TTTIO.h"
+#include "AI.h"
+
+struct node * ntree = NULL;
 
 int main( int argc, char ** argv )
 {
@@ -17,13 +20,32 @@ int main( int argc, char ** argv )
   brdx = 0;
   brdy = 0;
 
+  printf("populating tree!");
+  
+  ntree = malloc( sizeof *ntree );
+  if( ntree == NULL )
+    {
+      printf("Unable to allocate memory for tree!\n");
+      exit(EXIT_FAILURE);
+    }
+
   while( 1 )
     {
       print_board( brdx, brdy );
+
+      /* testing AI */
+      /*
+      if(! curr )
+	printf("BEST MOVE: %d\n", solve( brdx, brdy, TRUE ) );
+      */
+      /* end testing AI */
+      /*
       move = get_move( ( brdx | brdy ) );
+      */
 
       if( curr )
 	{
+	  move = get_move( ( brdx | brdy ) );
 	  /* O's move */
 	  SET( brdy, move );
 	  if( win( brdy ) )
@@ -36,6 +58,8 @@ int main( int argc, char ** argv )
       else
 	{
 	  /* X's move */
+	  move = solve( brdx, brdy, TRUE );
+
 	  SET( brdx, move );
 	  if( win( brdx ) )
 	    {
@@ -52,6 +76,8 @@ int main( int argc, char ** argv )
 	  return 0;
 	}
 
+
+      printf("\nmoves made: %d\n", num_moves( brdx | brdy ) );
       curr ^= 1;
     }
   return 0;
